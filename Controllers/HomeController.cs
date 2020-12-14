899bd4fe03cbfe36a8c0aa918628a1bfab6c031f
andrewlachapelle4 <66648434+andrewlachapelle4@users.noreply.github.com>
2020-12-13 20:58:40 -0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using S3_2.Models;
+
+namespace S3_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("Acceleration Profile.rdash");
+        }
+
+     
+        public IActionResult AccelerationProfile()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Reveal SDK/EmptyClass.cs
@@ -0,0 +1,34 @@
+﻿using Reveal.Sdk;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Demo1.RevealSDK
+{
+    public class RevealSdkContext : IRevealSdkContext
+    {
+        public IRVDataSourceProvider DataSourceProvider => null;
+
+        public IRVDataProvider DataProvider => null;
+
+        public IRVAuthenticationProvider AuthenticationProvider => null;
+
+        public Task<Stream> GetDashboardAsync(string dashboardId)
+        {
+            var resourceName = $"Demo1.Dashboards.{dashboardId}";
+            var assembly = Assembly.GetExecutingAssembly();
+            return Task.FromResult(assembly.GetManifestResourceStream(resourceName));
+        }
+
+        public Task SaveDashboardAsync(string userId, string dashboardId, Stream dashboardStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
+
+
